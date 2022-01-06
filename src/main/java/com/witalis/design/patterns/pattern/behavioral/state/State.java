8d7c3ded011b3e2e46f1c75648f73d0133ea4ebb
wellaxis <wellaxis@gmail.com>
2022-01-06 22:50:01 +0200

    behavioral/state definitions

--- src/main/java/com/witalis/design/patterns/pattern/behavioral/state/State.java
+++ src/main/java/com/witalis/design/patterns/pattern/behavioral/state/State.java
@@ -19,6 +19,29 @@ import lombok.extern.slf4j.Slf4j;
 )
 public class State implements IPattern {
 
+    /**
+     * Definition:
+     * -----------
+     * Visitor -> allows an object to change its behavior based on its state.
+     *     The state pattern is used in computer programming to encapsulate
+     *     varying behavior for the same object, based on its internal state.
+     *
+     * Algorithm:
+     * ----------
+     * 1. To create a state interface with required methods.
+     * 2. To create implementation classes of a state interface.
+     * 3. To create a basic interface for entity behaviour.
+     * 4. To create a context entity class with a state variable.
+     * 5. To set up an initial entity state & method to change state.
+     *
+     * Example:
+     * --------
+     * 1. State interface -> Charging { void charge(); }.
+     * 2. State classes -> [PowerUnit, USB, Wireless] { void charge(); }.
+     * 3. Entity interface -> Device { void use(); }
+     * 4. Entity abstract class -> Computer { private Charging charging; }
+     * 5. Entity classes -> [Laptop, Tablet, Mobile] { void changeCharge(); }
+     */
     @Override
     public void process() {
         log.info("=================== Pattern[state]: process =====================");
